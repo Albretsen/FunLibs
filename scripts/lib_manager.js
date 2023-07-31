38f4d617fa739dc34841143be1a127b5c1461970
Albretsen <asgeir.albretsen@gmail.com>
2023-07-31 14:53:54 +0200

    Added real libs to default libs

--- scripts/lib_manager.js
+++ scripts/lib_manager.js
@@ -45,6 +45,7 @@ export default class LibManager {
      */
     static async loadLibsToMemory() {
         LibManager.libs = await LibManager.getLibs();
+        console.log(JSON.stringify(LibManager.libs["yourLibs"]))
     }
 
     /**
