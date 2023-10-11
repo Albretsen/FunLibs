7e5398a3105140e1aaa0de119289975e894b6a75
Albretsen <asgeir.albretsen@gmail.com>
2023-10-11 15:08:13 +0200

    IAP Test

--- components/IAP.jsx
+++ components/IAP.jsx
@@ -1,34 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList } from 'react-native';
 import Purchases from 'react-native-purchases';
 
 const IAP = () => {
-    const [products, setProducts] = useState([]);
-    const [purchased, setPurchased] = useState(false);
-    const [error, setError] = useState("");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        // Fetch available products
-        const fetchProducts = async () => {
-            try {
-                console.log("FETCHING PRODUCTS:")
-                const { products } = await Purchases.getOfferings(); // Replace with your product IDs
-                console.log(JSON.stringify(products));
-                setProducts(products);
-            } catch (error) {
-                console.log('Error fetching products:', error);
-                setError(error.message);
-            }
-        };
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        console.log("FETCHING");
+        const productIdentifiers = ['test', 'videogames_pack']; // Replace with your product IDs
+        const fetchedProducts = await Purchases.getProducts(productIdentifiers);
+        console.log("FETCHED: " + JSON.stringify(fetchedProducts));
+        setProducts(fetchedProducts);
+      } catch (err) {
+        setError(err);
+      }
+    }
 
-        fetchProducts();
-    }, []);
+    fetchProducts();
+  }, []);
 
-    return (
-        <View>
-            {error && <Text>There was an error: {error}</Text>}
-        </View>
-    );
+  const handlePurchase = async (productId) => {
+    try {
+      const purchaseResponse = await Purchases.purchaseProduct(productId);
+      // Handle successful purchase here
+      console.log('Purchase was successful:', purchaseResponse);
+    } catch (err) {
+      console.error('Purchase failed:', err);
+    }
+  };
+
+  return (
+    <View style={{ padding: 20 }}>
+      {error && <Text>Error fetching products: {error.message}</Text>}
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.identifier}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 20 }}>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <Text>Price: {item.priceString}</Text>
+            <Button title="Buy" onPress={() => handlePurchase(item.identifier)} />
+          </View>
+        )}
+      />
+    </View>
+  );
 };
 
 export default IAP;
