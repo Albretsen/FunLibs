f241faeeb18a834733a182d0fa2e893ba85317b5
Hallvard Hetlelid <epichallvard@hotmail.com>
2023-10-26 18:43:33 +0200

    Removed bottom sheet Added Custom SegmentedButtons that allow for a gradient backgound

---
+++ components/SegmentedButtons.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+interface SegmentedButtonsProps {
+    buttons: Array<{
+        label: string;
+        id: string;
+        onPress: () => void;
+    }>
+}
+
+export default function SegmentedButtons({buttons}: SegmentedButtonsProps) {
+    const [activeButton, setActiveButton] = useState(0);
+    return(
+        <View style={styles.container}>
+            {buttons.map((button, index) => {
+                const activeGradient = ["#638BD5", "#60C195"];
+                const inactiveGradient = [segmentedButtonsBackgroundColor, segmentedButtonsBackgroundColor];
+                const active = activeButton == index;
+                return(
+                    <LinearGradient
+                        colors={active ? activeGradient : inactiveGradient}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 1 }}
+                        style={styles.buttonContainer}
+                    >
+                        <Pressable style={styles.buttonInnerContainer} onPress={() => {
+                            button.onPress();
+                            setActiveButton(index);
+                        }}>
+                            <View style={styles.labelContainer}>
+                                {active && (
+                                    <MaterialIcons name="check" size={20} color="white" />
+                                )}
+                                <Text style={[styles.label, {color: active ? 'white' : 'black'}]}>{button.label}</Text>
+                            </View>
+                        </Pressable>
+                    </LinearGradient>
+                )
+            })}
+        </View>
+    )
+}
+
+const segmentedButtonsBackgroundColor = 'white';
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+    },
+
+    buttonInnerContainer: {
+        height: 40,
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+
+    container: {
+        borderRadius: 20,
+        flex: 1,
+        flexDirection: 'row',
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#79747E',
+        columnGap: 1,
+        backgroundColor: '#79747E'
+    },
+
+    labelContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5
+    },
+
+    label: {
+        fontSize: 14,
+        color: 'white',
+        // Avoid whitespace above text
+        lineHeight: 0
+    }
+})
--- screens/LibsScreen.jsx
+++ screens/LibsScreen.jsx
@@ -7,12 +7,10 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 // ADS
 import { useIsFocused } from '@react-navigation/native';
 import { ScreenContext } from "../App";
-import BottomSheet, {BottomSheetScrollView} from '@gorhom/bottom-sheet';
 import Buttons from "../components/Buttons";
-import CustomBackground from "../components/CustomBackground";
 import { Divider } from '@rneui/themed';
-import FilterToggle from "../components/FilterToggle";
-import { SegmentedButtons, ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator } from "react-native-paper";
+import SegmentedButtons from "../components/SegmentedButtons";
 import FirebaseManager from "../scripts/firebase_manager";
 import Analytics from "../scripts/analytics";
 import { useTab } from "../components/TabContext";
@@ -452,43 +450,12 @@ export default function LibsScreen() {
 		}
 	}, [isFocused]);
 
-	useFocusEffect(
-		useCallback(() => {
-			setTimeout(() => {
-				bottomSheetRef?.current?.close();
-			}, 10);
-
-			return () => {
-			};
-		}, [])
-	);
-
-	const bottomSheetRef = useRef(null);
-
-	const handleOpenBottomSheet = () => {
-		bottomSheetRef.current?.snapToIndex(3);  // Or any other index, based on snapPoints array
-		setIsBottomSheetOpen(true);
-	};
-
-	const handleCloseBottomSheet = () => {
-		bottomSheetRef.current?.close();
-		setIsBottomSheetOpen(false)
-	};
-
-	const handleBottomSheetChange = useCallback((index) => {
-		if (index <= 0) { // Would be -1, but needs to account for first snap point being 1% due to hack fix
-			setIsBottomSheetOpen(false);
-		} else {
-			setIsBottomSheetOpen(true);
-		}
-	}, []);
-
 	const playReadToggle = (newValue) => {
+		console.log(newValue)
 		setPlayReadValue(newValue);
 		updateFilterOptions(newValue);
 	};
 
-	const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
 	const updateFilterOptions = (playableValue = playReadValue, categoryValue = selectedCategory, sortByValue = selectedSortBy, dateValue = selectedDate) => {
 		setEndReached(false);
@@ -501,7 +468,6 @@ export default function LibsScreen() {
 			playable: playableValue
 		};
 		loadListItems(filterOptions);
-		setIsBottomSheetOpen(false);
 	}
 
 	const { setTab } = useTab();
@@ -567,38 +533,35 @@ export default function LibsScreen() {
 					alignItems: "center",
 					gap: 10,
 					// padding: 2,
-					height: 40,
+					// height: 40,
 				}]}>
 					<SegmentedButtons
 						value={playReadValue}
 						onValueChange={playReadToggle}
-						style={{ width: 190, backgroundColor: "transparent" }}
-						density="small"
-						theme={{
-							colors: {
-								primary: '#49454F',
-								outline: "#79747E",
-								secondaryContainer: "#D1E8D5"
-							},
-						}}
 						buttons={[
 							{
-								label: i18n.t('play'),
-								value: true,
-								showSelectedCheck: true
-
+								label: i18n.t('newest'),
+								onPress: () => {
+									setSelectedSortBy("newest");
+									updateFilterOptions(playReadValue, undefined, "newest");
+								},
+								active: true,
 							},
 							{
-								label: i18n.t('read'),
-								value: false,
-								showSelectedCheck: true,
-							}
+								label: i18n.t('top'),
+								onPress: () => {
+									setSelectedSortBy("likes");
+									updateFilterOptions(playReadValue, undefined, "likes");
+								},
+							},
+							{
+								label: i18n.t('trending'),
+								onPress: () => {
+									setSelectedSortBy("trending");
+									updateFilterOptions(playReadValue, undefined, "trending");
+								},
+							},
 						]}
-					/>
-					<FilterToggle
-						open={handleOpenBottomSheet}
-						close={handleCloseBottomSheet}
-						isOpen={isBottomSheetOpen}
 					/>
 				</View>
 				<View>
@@ -699,106 +662,6 @@ export default function LibsScreen() {
 					)}
 				</>)}
 			</View>
-			<BottomSheet
-				ref={bottomSheetRef}
-				index={-1}
-				// Bug causes bottom sheet to reappear on navigation
-				// Kind of fixed with hack that sets it to the lowest snap point possible, then removes it after
-				// 10ms
-				snapPoints={['1%', '25%', '50%', '70%', '90%']}
-				enablePanDownToClose={true}
-				style={[{width: (Dimensions.get("window").width), paddingHorizontal: 20}]} // Required to work with the bottom navigation
-				backgroundComponent={CustomBackground}
-				onChange={handleBottomSheetChange}
-				onAnimate={(fromIndex, toIndex) => {
-					if (toIndex === -1) {
-						console.log('The bottom sheet is hidden');
-					} else {
-						console.log('The bottom sheet is shown');
-					}
-				}}
-			>
-			{/* TEST IF THIS WORKS IN EMULATOR, DOES NOT WORK ON WEB */}
-			{/* <BottomSheetScrollView> */}
-				<View>
-					<Text style={[ globalStyles.bold, {marginVertical: 6, fontSize: 20}]}>Sort by</Text>
-					<Buttons 
-						buttons={[
-							{
-								label: i18n.t('newest'),
-								icon: selectedSortBy === "newest" ? "done" : null,
-								buttonStyle: selectedSortBy === "newest" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null,
-								onPress: () => { setSelectedSortBy("newest"); updateFilterOptions(playReadValue, undefined, "newest"); }
-							},
-							{
-								label: i18n.t('top'),
-								icon: selectedSortBy === "likes" ? "done" : null,
-								buttonStyle: selectedSortBy === "likes" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null,
-								onPress: () => { setSelectedSortBy("likes"); updateFilterOptions(playReadValue, undefined, "likes"); }
-							},
-							{
-								label: i18n.t('trending'),
-								icon: selectedSortBy === "trending" ? "done" : null,
-								buttonStyle: selectedSortBy === "trending" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null,
-								onPress: () => { setSelectedSortBy("trending"); updateFilterOptions(playReadValue, undefined, "trending"); }
-							}
-						]}
-						buttonStyle={{borderRadius: 10, borderColor: "#454247", backgroundColor: "#F0F1EC", minWidth: 50, height: 45}}
-						containerStyle={{justifyContent: "flex-start", gap: 20}}
-						labelStyle={{fontSize: 14, fontWeight: 500}}
-					/>
-					{/* <Divider color="#CAC4D0" style={{marginVertical: 10}}/>
-					<Text style={[ globalStyles.bold, {marginVertical: 6, fontSize: 20}]}>Date</Text>
-					<Buttons 
-						buttons={[
-							{
-								label: "All time",
-								icon: selectedSortBy === "trending" ? "done" : (selectedDate === "allTime" ? "done" : null),
-								buttonStyle: selectedSortBy === "trending" 
-									? {borderColor: "transparent", backgroundColor: "#D1E8D5"} 
-									: (selectedDate === "allTime" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null),
-								onPress: () => { setSelectedDate("allTime"); updateFilterOptions(playReadValue, undefined, undefined, "allTime"); }
-							},
-							{
-								label: "Today",
-								icon: selectedSortBy === "trending" ? null : (selectedDate === "today" ? "done" : null),
-								buttonStyle: selectedSortBy === "trending" 
-									? { backgroundColor: "#CFD8DC" } 
-									: (selectedDate === "today" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null),
-								onPress: () => { setSelectedDate("today"); updateFilterOptions(playReadValue, undefined, undefined, "today"); }
-							},
-							{
-								label: "This week",
-								icon: selectedSortBy === "trending" ? null : (selectedDate === "thisWeek" ? "done" : null),
-								buttonStyle: selectedSortBy === "trending" 
-									? { backgroundColor: "#CFD8DC" } 
-									: (selectedDate === "thisWeek" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null),
-								onPress: () => { setSelectedDate("thisWeek"); updateFilterOptions(playReadValue, undefined, undefined, "thisWeek"); }
-							},
-							{
-								label: "This month",
-								icon: selectedSortBy === "trending" ? null : (selectedDate === "thisMonth" ? "done" : null),
-								buttonStyle: selectedSortBy === "trending" 
-									? { backgroundColor: "#CFD8DC" } 
-									: (selectedDate === "thisMonth" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null),
-								onPress: () => { setSelectedDate("thisMonth"); updateFilterOptions(playReadValue, undefined, undefined, "thisMonth"); }
-							},
-							{
-								label: "This year",
-								icon: selectedSortBy === "trending" ? null : (selectedDate === "thisYear" ? "done" : null),
-								buttonStyle: selectedSortBy === "trending" 
-									? { backgroundColor: "#CFD8DC" } 
-									: (selectedDate === "thisYear" ? {borderColor: "transparent", backgroundColor: "#D1E8D5"} : null),
-								onPress: () => { setSelectedDate("thisYear"); updateFilterOptions(playReadValue, undefined, undefined, "thisYear"); }
-							},
-						]}
-						buttonStyle={{borderRadius: 10, borderColor: "#454247", backgroundColor: "#F0F1EC", minWidth: 50, height: 40}}
-						containerStyle={{justifyContent: "flex-start", gap: 20}}
-						labelStyle={{fontSize: 14, fontWeight: 500}}
-					/>
-					<Divider color="#CAC4D0" style={{marginVertical: 10}}/> */}
-				</View>
-			</BottomSheet>
 	  	</SafeAreaView>
 	);
 }
